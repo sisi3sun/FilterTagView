bff2f0bdc8e2ada5e440d7ae86fdfd8b3e055aa5
龙兮 <xi.wangxx@alibaba-inc.com>
2014-08-19 14:45:08 +0800

    Initial Commit

---
+++ FilterTagView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FilterTagView
+//
+//  Created by xi.wangxx on 14-8-19.
+//  Copyright (c) 2014年 sisi3sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FilterTagView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FilterTagView
+//
+//  Created by xi.wangxx on 14-8-19.
+//  Copyright (c) 2014年 sisi3sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
